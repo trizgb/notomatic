e16eb09f80d966911e0387bab3bcfc0883b1887c
Beatriz Gomez <beatrizgomez@paradigmadigital.com>
2024-06-11 11:02:22 +0200

    feat/validations: validate form inputs

--- src/pages/NoteCreate.js
+++ src/pages/NoteCreate.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { createNewNote } from 'api'
@@ -10,6 +10,28 @@ const NoteCreate = () => {
   const { t } = useTranslation('translation')
   const navigate = useNavigate()
   const [form, setForm] = useState({ title: '', content: '' })
+  const [errors, setErrors] = useState({ title: '', content: '' })
+
+  const validate = values => {
+    if (values.title) {
+      if (values.title.length < 3) {
+        setErrors({ ...errors, title: t('form.textfield-error-1') })
+      } else if (values.title.length > 3 && values.title.length < 20) {
+        setErrors({ ...errors, title: '' })
+      } else if (values.title.length > 20) {
+        setErrors({
+          ...errors,
+          title: t('form.textfield-error-2'),
+        })
+      }
+    }
+
+    if (values.content) {
+      if (values.content.length < 3) {
+        setErrors({ ...errors, content: t('form.textarea-error-1') })
+      } else setErrors({ ...errors, content: '' })
+    }
+  }
 
   const handleChange = e => {
     setForm({ ...form, [e.currentTarget.name]: e.currentTarget.value })
@@ -22,22 +44,36 @@ const NoteCreate = () => {
     }).finally(() => navigate('/'))
   }
 
+  useEffect(() => {
+    validate(form)
+  }, [form])
+
+  console.log(form.title.length, form.content.length)
+
   return (
     <section className="section-wrapper">
       <Form
         title={t('note-create.title')}
         submitText={t('form.submit-save')}
         onSubmit={handleSubmit}
+        isSubmitDisabled={
+          form.title === '' ||
+          form.content === '' ||
+          errors.title !== '' ||
+          errors.content !== ''
+        }
       >
         <TextField
           label={t('form.textfield-label')}
           name="title"
           onChange={handleChange}
+          error={errors.title}
         />
         <TextArea
           label={t('form.textarea-label')}
           name="content"
           onChange={handleChange}
+          error={errors.content}
         />
       </Form>
     </section>
