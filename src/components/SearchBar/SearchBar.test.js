68f7a8db3eb1bdf8559fb813b9367813289eaff0
Beatriz Gomez <beatrizgomez@paradigmadigital.com>
2024-06-24 12:48:24 +0200

    Fix search bar test

--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -6,14 +6,14 @@ describe('SearchBar', () => {
     render(<SearchBar />)
 
     expect(
-      screen.getByPlaceholderText('search.placeholder'),
+      screen.getByPlaceholderText('common.search-placeholder'),
     ).toBeInTheDocument()
   })
 
   it('updates on change', () => {
     render(<SearchBar />)
 
-    const input = screen.getByPlaceholderText('search.placeholder')
+    const input = screen.getByPlaceholderText('common.search-placeholder')
     fireEvent.change(input, { target: { value: 'test' } })
     expect(input.value).toBe('test')
   })
